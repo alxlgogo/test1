7e0bdb73553bd7c630a88ab04a393af6398305a0
alxlgogo <alxlgogo@gmail.com>
2020-10-04 20:21:09 +0100

    2223

--- src/com/lwj/Group.java
+++ src/com/lwj/Group.java
@@ -3,6 +3,6 @@ package com.lwj;
 public class Group {
     public static void main(String[] args) {
         System.out.println("第一次修改");
-        System.out.println("第二次修22改12dA");
+        System.out.println("第二次2修22改12dA");
     }
 }
